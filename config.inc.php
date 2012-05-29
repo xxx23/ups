41aa349bb9380837028910f7cedf9e49e9586750
carlcarl <carlcarlking@gmail.com>
2012-05-29 23:14:48 +0800

    Update config template

--- config.inc.php
+++ config.inc.php
@@ -1,21 +1,24 @@
 <?php 
 //System Begin Time
-	$SYSTEM_BEGIN_YEAR = "2008";
-	$SYSTEM_BEGIN_MONTH = "04";
-	$SYSTEM_BEGIN_DAY = "16";
+	// ini_set('display_errors',1);
+	// error_reporting(E_ALL);
+
+	$SYSTEM_BEGIN_YEAR = "2012";
+	$SYSTEM_BEGIN_MONTH = "05";
+	$SYSTEM_BEGIN_DAY = "24";
 
 //Web Server Setting
-	$HOST = "140.123.105.16";
-	$HOMEURL = "http://140.123.105.16/";
-	$WEBROOT = "/~tkraha/";
-	$HOME_PATH = "/home/tkraha/WWW/";
+	$HOST = "140.123.230.140";
+	$HOMEURL = "http://140.123.230.140/";
+	$WEBROOT = "/";
+	$HOME_PATH = "/var/www/";
 
 //database setting
 	$DB_TYPE = "mysql";
 	$DB_HOST = "localhost";
-	$DB_NAME = "elearning2009";
-	$DB_USERNAME = "elearning2009";
-	$DB_USERPASSWORD = "elearning2009";
+	$DB_NAME = "elearning";
+	$DB_USERNAME = "hsng";
+	$DB_USERPASSWORD = "hsng@root";
 
 //CSS
 	$CSS_PATH = "css/";
@@ -31,23 +34,23 @@
 
 	$LIBRARY_PATH = "library/";
 //Teaching materials
-	$DATA_FILE_PATH = $HOME_PATH."Data_File/";
+	$DATA_FILE_PATH = "/var/www/Data_File/";
 //Media for streaming
-	$MEDIA_FILE_PATH = $HOME_PATH."Streaming_File/";
+	$MEDIA_FILE_PATH = "/var/www/Streaming_File/";
 //Course file
-	$COURSE_FILE_PATH = $HOME_PATH."Course_File/";
+	$COURSE_FILE_PATH = "/var/www/Course_File/";
 //Personal Directory
-	$PERSONAL_PATH = $HOME_PATH."Personal_File/";
-	$WWW_GID = "80";
+	$PERSONAL_PATH = "/var/www/Personal_File/";
+	$WWW_GID = "33";
 
 //FTP setting
-	$FTP_IP = "140.123.105.16";
+	$FTP_IP = "140.123.230.140";
 	$FTP_PORT = "21";
 	$MAX_UPLOAD_SIZE = "128";
 
 //utility settings
-	$RAR_PATH = "/usr/local/bin/rar";
-	$PHP_PATH = "/usr/local/bin/php";
+	$RAR_PATH = "/usr/bin/rar";
+	$PHP_PATH = "/usr/bin/php";
 
 //MAIL settings
 	$MAIL_SMTP_HOST = "localhost";
@@ -55,18 +58,12 @@
 	$MAIL_ADMIN_USER = "";
 	$MAIL_ADMIN_PASSWROD = "";
 	$MAIL_ADMIN_EMAIL = "";
-	$MAIL_ADMIN_EMAIL_NICK = "[中正大學-教學平台管理者]";
-    
-//NKNU SQL server MSSQL connect setting
-    $NKNU_DB_HOST = 'NKNUDB';
-    $NKNU_DB_USER = 'Hsngccu';
-    $NKNU_DB_PASSWD = 'Cyber3elearning';
-    $NKNU_DATABASE = 'course_Hsngccu';
-    $NKNU_TABLE = 'course';
+	$MAIL_ADMIN_EMAIL_NICK = "[教育部-教學平台管理者]";
 
+	$USE_MYSQL = true;
+	$USE_MONGODB = !$USE_MYSQL;
 
-
-    set_include_path( get_include_path() . PATH_SEPARATOR . $HOME_PATH.$LIBRARY_PATH."Smarty". PATH_SEPARATOR . $HOME_PATH.$LIBRARY_PATH."PearDB");
+	set_include_path( get_include_path() . PATH_SEPARATOR . $HOME_PATH.$LIBRARY_PATH."Smarty". PATH_SEPARATOR . $HOME_PATH.$LIBRARY_PATH."PearDB");
 	//Pear DB library
 	require_once("DB.php");
 	
@@ -83,13 +80,31 @@
 	    'debug'       => 2,
 	    'portability' => DB_PORTABILITY_ALL,
 	);
-	
-	$DB_CONN = DB::connect($dsn, $options);
-	if (PEAR::isError($DB_CONN))	die($DB_CONN->getMessage());
 
-	//appended by puppy for avoiding encoding problem
-	if ($DB_TYPE == "mysql")
-	      $DB_CONN->query("SET NAMES 'utf8'");
+	if($USE_MYSQL)
+	{
+		$DB_CONN = DB::connect($dsn, $options);
+		if (PEAR::isError($DB_CONN))	die($DB_CONN->getMessage());
+
+		//appended by puppy for avoiding encoding problem
+		if ($DB_TYPE == "mysql")
+			  $DB_CONN->query("SET NAMES 'utf8'");
+	}
+	else if($USE_MONGODB)
+	{
+		try 
+		{
+			$m = new Mongo(); // connect
+			$db = $m->selectDB($DB_NAME);
+			$db->authenticate("hsng", "hsng@root");
+		}
+		catch(MongoConnectionException $e) 
+		{
+			echo '<p>Couldn\'t connect to mongodb, is the "mongo" process running?</p>';
+			exit();
+		}
+	}
+	
 		    
 	//Smarty library
 	require_once("Smarty.class.php");
@@ -121,9 +136,10 @@
 	//big5 library
 	require_once($LIBRARY_PATH . "big5_func/big5_func.inc");
 	
+	
 	//Learning Tracking library
 	require_once($LIBRARY_PATH . "learningTracking.php");
-	
+    
 	//Learning Tracking Setting
 	$NOISE_TIME = 5;	//seconds
 ?>

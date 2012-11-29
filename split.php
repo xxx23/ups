a5ea5e8cebc420b21a4433f3f52a9cda0ba7a0a8
carlcarl <carlcarlking@gmail.com>
2012-11-29 13:55:57 +0800

    update presplit chunk code

--- split.php
+++ split.php
@@ -2,24 +2,51 @@
 $DB_USERNAME = "hsng";
 $DB_USERPASSWORD = "ringline";
 
-$m = new Mongo("mongodb://${DB_USERNAME}:${DB_USERPASSWORD}@localhost/elearning");
+$m = new Mongo();
 
 $admin = $m->admin;
-$range = 100000;
-for($i = 0; $i < 9; $i++)
+// $range = 10000;
+// for($i = 0; $i < 9; $i++)
+// {
+// 	$split = array(
+// 		'split' => 'elearning.login_log',
+// 		'middle' => array('p' => ($i + 1) * $range)
+// 	);
+// 	$res = $admin->command($split);
+// 
+// 	$shardName = 'shard000' . strval(intval($i % 3));
+// 	$move = array(
+// 		'moveChunk' => 'elearning.login_log',
+// 		'find' => array('p' => ($i + 1) * $range),
+// 		'to' => $shardName
+// 	);
+// 	$res = $admin->command($move);
+// }
+// for ( var x=97; x<97+26; x++ ){
+//   for( var y=97; y<97+26; y+=6 ) {
+//     var prefix = String.fromCharCode(x) + String.fromCharCode(y);
+//     $admin->command( { split : "myapp.users" , middle : { email : prefix } } );
+//   }
+// }
+
+for($i = 48; $i <= 90; $i++)
 {
-	$split = array(
-		'split' => 'elearning.online_number_s',
-		'middle' => array('_id' => ($i + 1) * $range)
-	);
-	$res = $admin->command($split);
+	// for($j = 97; $j < 97+26; $j++)
+	// {
+		$prefix = chr($i);
+		if($i >= 65) $prefix = strtolower($prefix);
+		$split = array(
+			'split' => 'elearning.login_log',
+			'middle' => array('_id' => $prefix)
+		);
+		$res = $admin->command($split);
 
-	$shardName = 'shard000' . strval(intval($i / 3));
-	$move = array(
-		'moveChunk' => 'elearning.online_number_s',
-		'find' => array('_id' => ($i + 1) * $range),
-		'to' => $shardName
-	);
-	$res = $admin->command($move);
+		$shardName = 'shard000' . strval(intval(($i - 48) % 3));
+		$move = array(
+			'moveChunk' => 'elearning.login_log',
+			'find' => array('_id' => $prefix),
+			'to' => $shardName
+		);
+		$res = $admin->command($move);
+	// }
 }
-

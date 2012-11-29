20323d6d7546b8e7a6266c85fe23262cc51e2b34
carlcarl <carlcarlking@gmail.com>
2012-11-29 13:38:50 +0800

    Remove space in code

--- library/date.php
+++ library/date.php
@@ -1,4 +1,4 @@
-<?php	
+<?php
 	//將字串格式的字串轉成秒數 zqq
 	//用來比較時間
 	function str_to_time($str_time)
@@ -23,7 +23,7 @@
 		$sec	= $date_n['seconds'];			
 		return mktime($hour,$min,$sec,$month,$day,$year);
 	}
-	
+
 	//取得現在時間 轉成 字串 zqq
 	function get_now_time_str()
 	{
@@ -36,4 +36,4 @@
 		$sec	= $date_n['seconds'];			
 		return date("Y-m-d H:i:s",mktime($hour,$min,$sec,$month,$day,$year));
 	}
-?>	
+?>
